60ed1e840034b18a9d7b8ccdab85a250226af0e8
Danial <bashirzadehdanial@gmail.com>
2024-07-17 23:15:47 +0100

    Add Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Currency Converter!");
+            Console.WriteLine("Example currencies: USD, EUR, GBP");
+            Console.Write("Enter the base currency: ");
+            string baseCurrency = Console.ReadLine().ToUpper();
+
+            Console.Write("Enter the target currency: ");
+            string targetCurrency = Console.ReadLine().ToUpper();
+
+            Console.Write("Enter the amount to convert: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+            CurrencyConverter converter = new CurrencyConverter();
+            decimal convertedAmount = await converter.ConvertCurrency(amount, baseCurrency, targetCurrency);
+
+            Console.WriteLine($"{amount} {baseCurrency} is {convertedAmount} {targetCurrency}.");
+        }
+    }
+}
